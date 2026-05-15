ebb3cc56d76cb554074ed83051ffefee744174ec
Agnieszka <am310943@student.polsl.pl>
2026-05-15 03:39:15 +0200

    dodane do paska

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import {BrowserRouter,Routes,Route,Link,useLocation} from "react-router-dom";
-import ONas from "./Strony/ONas";
-import AdminPanel from "./Strony/AdminPanel";
-import StronaGlowna from "./Strony/StronaGlowna";
 import "./App.css";
 import Navbar from "./Components/Navbar.js";
 
--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {BrowserRouter,Routes,Route,Link,useLocation} from "react-router-dom";
-import ONas from "../Strony/ONas";
+import ONas from "../Strony/OCzasopismie";
 import AdminPanel from "../Strony/AdminPanel";
 import StronaGlowna from "../Strony/StronaGlowna";
 import Login from "../Strony/AdminLogowanie";
-import 
+import InfoDlaAut from "../Strony/InfoDlaAut";
+import ProcesRecenzji from "../Strony/ProcesRecenzji";
+import Recenzenci from "../Strony/Recenzenci";
+import Redakcja from "../Strony/Redakcja";
+import Roczniki from "../Strony/Roczniki";
+
 function Navbar () {
   const location = useLocation();
 
@@ -18,15 +23,24 @@ function Navbar () {
       {!hideNavbar && (
         <nav className="navbar">
           <Link to="/">Strona Główna</Link> |{" "}
-          <Link to="/onas">O Nas</Link>
+          <Link to="/o-nas">O Czasopismie</Link> |{" "}
+          <Link to="/proces-recenzji">Proces Recenzji</Link> |{" "}
+          <Link to="/recenzenci">Recenzenci</Link> |{" "}
+          <Link to="/redakcja">Redakcja</Link> |{" "}
+          <Link to="/roczniki">Roczniki</Link>
         </nav>
       )}
 
       <Routes>
         <Route path="/" element={<StronaGlowna />} />
-        <Route path="/onas" element={<ONas />} />
+        <Route path="/o-nas" element={<ONas />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/admin/logowanie" element={<Login />} />
+        <Route path="/info-dla-autorow" element={<InfoDlaAut />} />
+        <Route path="/proces-recenzji" element={<ProcesRecenzji />} />
+        <Route path="/recenzenci" element={<Recenzenci />} />
+        <Route path="/redakcja" element={<Redakcja />} />
+        <Route path="/roczniki" element={<Roczniki />} />
       </Routes>
     </>
   );
