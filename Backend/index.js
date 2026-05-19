350209bc47f9a0c5c39f2e8cca370cf5894ac05b
Weronika <weronikaskowron1@gmail.com>
2026-05-19 17:02:04 +0200

    wyszukiwanie działa

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.get("/api/test", (req, res) => {
   res.json({
-    message: "Backend działa 🚀",
+    message: "Backend lololo działa 🚀",
   });
 });
 
@@ -48,8 +48,58 @@ app.get("/api/articles", async (req, res) => {
   }
 });
 
+// app.get("/api/szukaj", (req, res) => {
+//   res.json({
+//     message: "SZUKAJ DZIAŁA",
+//     q: req.query.q,
+//   });
+// });
+
+app.get("/api/search", async (req, res) => {
+  try {
+    const search = req.query.q || "";
+
+    const result = await pool.query(
+      `
+      SELECT
+        articles.id,
+        title,
+        pages_from,
+        pages_to,
+        publication_date,
+        pdf_path,
+        extra_file_path,
+        category,
+        symbol,
+        string_agg(a.name || ' ' || a.surname, ', ' ORDER BY a.id) AS authors
+      FROM articles
+      LEFT JOIN categories
+        ON id_category = categories.id
+      LEFT JOIN author_articles aa
+        ON aa.id_article = articles.id
+      LEFT JOIN authors a
+        ON aa.id_author = a.id
+      WHERE
+        title ILIKE $1
+        OR category ILIKE $1
+        OR a.name ILIKE $1
+        OR a.surname ILIKE $1
+      GROUP BY articles.id, category, symbol
+      ORDER BY publication_date DESC;
+      `,
+      [`%${search}%`]
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Błąd serwera" });
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  console.log("dzsvsdv");
   console.log(`Server działa na porcie ${PORT}`);
 });
--- Frontend/src/Strony/Szukaj.js
+++ Frontend/src/Strony/Szukaj.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+
+import Article from "../Components/Article";
 
 function Szukaj() {
   const location = useLocation();
@@ -7,12 +9,25 @@ function Szukaj() {
   const params = new URLSearchParams(location.search);
 
   const query = params.get("q");
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/search?q=${query}`)
+      .then((res) => res.json())
+      .then((data) => setArticles(data))
+      .catch((err) => console.error(err));
+  }, [query]);
 
   return (
     <div style={{ padding: "40px" }}>
       <h1>Wyniki wyszukiwania</h1>
 
       <p>Szukasz: {query}</p>
+      <div className="blog-grid">
+        {articles.map((article) => (
+          <Article key={article.id} article={article} />
+        ))}
+      </div>
     </div>
   );
 }
